dabe16c0882824345fb0fa54a31a26bd63a3529a
easyacards <tim@easycards.org>
2012-12-23 19:52:39 +0400

    migration to 2.1 changes

--- project/Build.scala
+++ project/Build.scala
@@ -1,21 +1,19 @@
+import play.Project._
 import sbt._
 import Keys._
-import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "EasyCards"
-    val appVersion      = "1.0"
+  val appName = "EasyCards"
+  val appVersion = "1.0"
 
 
-    val appDependencies = Seq(
-      anorm,
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
-    )
-
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "stylesheets" / "bootstrap" ** "main.less")
-
-    )
-
+  val appDependencies = Seq(
+    jdbc,
+    anorm,
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+  )
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "stylesheets" / "bootstrap" ** "main.less")
+  )
 }
